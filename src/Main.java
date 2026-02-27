6b902ea7179e717cacc4025fa126986e40ac9ed0
Geovani237 <geovanicsouza7@gmail.com>
2026-02-27 07:27:36 -0300

    feat: adiciona classes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import br.github.impostos.EmpresaLucroReal;
+import br.github.impostos.EmpresaSimples;
 import br.github.impostos.GestorDeImpostos;
 import br.github.impostos.PessoaFisica;
 
@@ -6,11 +8,16 @@ public class Main {
 
         var gestorImpostos = new GestorDeImpostos();
 
-        var joao = new PessoaFisica("Joao", 45_000);
+        var joao = new PessoaFisica("Joao", 45_500);
         var maria = new PessoaFisica("Maria", 180_000);
+
+        var barDoZe = new EmpresaSimples("Bar do Zé", 250_000, 160_000);
+        var codeConsultoria = new EmpresaLucroReal("Code Consultoria", 8_000_000, 6_000_000);
 
         gestorImpostos.adicionar(joao);
         gestorImpostos.adicionar(maria);
+        gestorImpostos.adicionar(barDoZe);
+        gestorImpostos.adicionar(codeConsultoria);
 
         System.out.printf("Total de impostos: %.2f", gestorImpostos.getValorTotalImpostos());
     }
---
+++ src/br/github/impostos/EmpresaSimples.java
@@ -0,0 +1,15 @@
+package br.github.impostos;
+
+public class EmpresaSimples extends PessoaJuridica{
+    public double ALIQUOTA_IMPOSTO_FATURAMENTO = 0.06;
+
+    public EmpresaSimples(String nome, double faturamentoAnual, double despesaAnual) {
+        super(nome, faturamentoAnual, despesaAnual);
+    }
+
+
+    @Override
+    public double calcularImpostos() {
+        return getFaturamentoAnual() * ALIQUOTA_IMPOSTO_FATURAMENTO;
+    }
+}
